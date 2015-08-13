3ee484db493c1e39a0aa4c6be6553e04fb665b14
陈 力 <robble@chendeMac-mini.local>
2015-08-13 08:05:25 +0800

    增加test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Copyright (c) 2013 DY.Feng <yyfeng88625@gmail.com>
+# All rights reserved
+
+# pylint: disable=R0904
+
+"""工具集.
+
+
+"""
+
+#$Id$
+__author__ = [
+    'robble <hymm91@gmail.com>',
+]
+__version__ = '$Revision: 0.1 $'
+
+
+import os
+
+
+if __name__ == '__main__':
+
+    print 'hello github.'
+
+
+
