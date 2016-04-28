c3801a3da30299787a54388251ea06ae50045024
yghDaydayUp <ygh7573418@163.com>
2016-04-28 16:45:55 +0800

    finish discuss

--- discuss.js
+++ discuss.js
@@ -1,6 +1,28 @@
-(function () {
-	function sayHi() {
-		console.log("Hi, git!");
-	}
-	sayHi();
-})();
++ (void)homeMenuDataSuccess:(void(^)(HomeMenuData *homeMenuData))success failure:(void(^)(NSError *error))failure{
+
+    
+    NSString *urlStr = [NSString stringWithFormat:@"%@/api/App/GetHomeIndexCategory",INTEGRALURL];
+    // GET方法
+    [ECGOHttpTool GET:urlStr parameters:nil success:^(id result) {
+        
+       NSLog(@"%@",result);
+        
+        
+        HomeMenuList *homeMenulist = [HomeMenuList objectWithKeyValues:result];
+        
+        if (success) {
+            success(homeMenulist.data);
+        }
+        
+        
+    } failure:^(NSError *error) {
+        
+        if (failure) {
+            failure(error);
+        }
+        
+    }];
+
+
+}
+
